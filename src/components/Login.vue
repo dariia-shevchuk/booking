e22862646ffc43247c2314a318ac8c0828f8ca79
Dariia Shevchuk <dashamensha@gmail.com>
2023-06-27 16:29:14 +0200

    Added Sign and Log in pages

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,3 +1,86 @@
 <template>
-    <h1>Login</h1>
+  <div class="login-container">
+    <h2>Login</h2>
+    <form @submit="login" class="login-form">
+      <input type="text" v-model="username" placeholder="Username/Email" required>
+      <input type="password" v-model="password" placeholder="Password" required>
+      <button type="submit">Login</button>
+    </form>
+    <p v-if="errorMessage" class="error-message">{{ errorMessage }}</p>
+  </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      errorMessage: ''
+    }
+  },
+  methods: {
+    login(event) {
+      event.preventDefault()
+      axios.post('/api/login', {
+        username: this.username,
+        password: this.password
+      })
+      .then(response => {
+        localStorage.setItem('token', response.data.token)
+        this.$router.push('/dashboard')
+      })
+      .catch(error => {
+        this.errorMessage = 'Invalid username/email or password'
+        console.error(error)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+.login-container {
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+h2 {
+  text-align: center;
+  margin-bottom: 20px;
+  color: #333;
+}
+
+.login-form {
+  display: flex;
+  flex-direction: column;
+}
+
+input {
+  margin-bottom: 10px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+button {
+  padding: 10px 20px;
+  background-color: #4682B4;
+  color: white;
+  border: none;
+  cursor: pointer;
+  border-radius: 4px;
+}
+
+button:hover {
+  background-color: #21637F;
+}
+
+.error-message {
+  color: red;
+  text-align: center;
+}
+</style>
--- src/components/Sign.vue
+++ src/components/Sign.vue
@@ -1,3 +1,99 @@
 <template>
-    <h1>Sign up</h1>
+  <div class="register-container">
+    <h2 class="register-title">Register</h2>
+    <form @submit="register" class="register-form">
+      <input type="text" v-model="username" placeholder="Username" class="register-input" required>
+      <input type="email" v-model="email" placeholder="Email" class="register-input" required>
+      <input type="password" v-model="password" placeholder="Password" class="register-input" required>
+      <button type="submit" class="register-button">Register</button>
+    </form>
+    <p v-if="errorMessage" class="error-message">{{ errorMessage }}</p>
+  </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      username: '',
+      email: '',
+      password: '',
+      errorMessage: ''
+    }
+  },
+  methods: {
+    register(event) {
+      event.preventDefault()
+      axios.post('/api/register', {
+        username: this.username,
+        email: this.email,
+        password: this.password
+      })
+      .then(response => {
+        console.log('Registration successful:', response.data)
+        this.$router.push('/login')
+      })
+      .catch(error => {
+        this.errorMessage = 'Registration failed. Please try again.'
+        console.error(error)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+.register-container {
+
+  max-width: 400px;
+  
+  margin: 0 auto;
+  padding: 20px;
+  //background-color: #f5f5f5;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.register-title {
+  text-align: center;
+  margin-bottom: 20px;
+  color: #333;
+  font-size: 24px;
+}
+
+.register-form {
+  display: flex;
+  flex-direction: column;
+}
+
+.register-input {
+  margin-bottom: 10px;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+}
+
+.register-button {
+  padding: 12px 24px;
+  background-color: #4682B4;
+  color: white;
+  border: none;
+  cursor: pointer;
+  border-radius: 4px;
+  font-size: 16px;
+  transition: background-color 0.3s;
+}
+
+.register-button:hover {
+  background-color: #21637F;
+}
+
+.error-message {
+  color: red;
+  text-align: center;
+  font-size: 14px;
+}
+</style>
